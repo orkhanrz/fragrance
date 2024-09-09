effc82c8352ec31eeb3a934469f1be8f6ef97ab6
Orkhan Rzali <orkhan.rz8@gmail.com>
2024-09-09 11:53:11 +0400

    init

--- fragrance/obj/Debug/net8.0/fragrance.AssemblyInfo.cs
+++ fragrance/obj/Debug/net8.0/fragrance.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fragrance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5275ab800140699e5401d42ec47b38851e5e2c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("fragrance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fragrance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
